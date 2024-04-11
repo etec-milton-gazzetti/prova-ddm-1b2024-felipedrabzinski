a64501f7a731146fe3abff7a6dd05a8333992e85
Felipe Drabzinski <134696756+felipedrabzinski@users.noreply.github.com>
2024-04-11 15:44:58 +0000

    quarto commit

--- screens/sobre.js
+++ screens/sobre.js
@@ -6,21 +6,28 @@ export default function Sobre({ navigation }) {
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Sobre</Text>
       <Text style={styles.text}>Luís Felipe de Lima Drabzinski</Text>
+      <Text style={styles.text}>3º Info</Text>
 
-      <Pressable style={{ padding: 10, backgroundColor: '#000000', borderRadius: 5, marginTop: 10 }} onPress={() => navigation.navigate("Home")}><Text style={{ color: 'white' }}>Voltar</Text></Pressable>
+      <Pressable style={{ padding: 10, backgroundColor: '#000000', borderRadius: 5, marginTop: 10, width: '50%', alignSelf: 'center', alignItems: 'center' }} onPress={() => navigation.navigate("Home")}><Text style={{ color: 'white' }}>Voltar</Text></Pressable>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  content: {
     alignItems: 'center',
     padding: 20,
   },
+
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'center',
   },
   image: {
     width: 150,
